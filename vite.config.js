74e262ce68d9a9a191cc19805670cb0600aa36f3
mostafa <mostafaghr12345@gmail.com>
2025-07-19 15:09:01 +0330

    Configure for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/kharjNegar/',
   plugins: [react(), tailwindcss(),
   VitePWA({
     registerType: 'autoUpdate',
